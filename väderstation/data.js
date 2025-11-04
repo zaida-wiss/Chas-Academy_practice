71027c065cfab3cb09295ee630dbca48caf07f15
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-11-04 13:11:24 +0100

    nu har jag påbörjat ui.js

--- väderstation/data.js
+++ väderstation/data.js
@@ -1,6 +1,6 @@
     export async function getWeather(city) {
-     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=59.33&lon=18.06&appid=d6f3a1a45529ce0b80572cc15116ba25
-&units=metric&lang=sv")
+     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=sv&appid=d6f3a1a45529ce0b80572cc15116ba25
+`)
   const data= await response.json();
   return data;
     }
