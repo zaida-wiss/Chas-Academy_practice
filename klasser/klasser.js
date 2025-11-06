5124f4aa49a17be5f57d4b3d4cc65069fb4a30ee
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-11-06 13:27:00 +0100

    testade lägga till saldo

--- klasser/klasser.js
+++ klasser/klasser.js
@@ -16,3 +16,61 @@ class bankaccount {
     }
     
 }
+
+// Skapar två nya konton med kontonummer och startsaldo
+let pippi = new bankaccount("12345", 1000);
+let zaida = new bankaccount("67890", 500);
+
+
+zaida.deposit(1000);    // Zaida sätter in 1000 kr
+zaida.withdraw(300);    // Zaida tar ut 300 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+zaida.deposit(3000);    // Zaida sätter in 1000 kr
+pippi.deposit(300000);    // Pippi sätter in 1000 kr
+zaida.showBalance();    // Visar saldot
+pippi.showBalance();    // Visar saldot
