a16c82818da37511cf70caf421b0222ad8102400
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-11-07 22:56:06 +0100

    nu fungerar även Set Interval

--- boilerroom v.45/main.js
+++ boilerroom v.45/main.js
@@ -6,11 +6,22 @@ const weatherOutput =document.createElement("p");
 
 //Funktion som visar vädret
 const showWeather= () => {
-    document.querySelector("#citySearch").appendChild(weatherOutput);
-    const city =  cityInput.value.charAt(0).toUpperCase() + cityInput.value.slice(1).toLowerCase();
-    weatherOutput.textContent = findCityWeather
-    console.log ("vädret visades");
-};
+    document.querySelector("#svar").appendChild(weatherOutput);
+    const city =  cityInput.value.charAt(0).toUpperCase() + 
+                  cityInput.value.slice(1).toLowerCase();
+    const weather = findCityWeather(city);
+    if (!weather) {
+      weatherOutput.innerHTML="<p>Staden finns inte i databasen. </p>";
+      console.log("något blev fel");
+      return;
+    }
+      weatherOutput.innerHTML = `
+      <p>${weather.stad}</p>
+      <p>${weather.temperatur}°C</p>
+      <p>${weather.description}</p>`;
+      console.log ("vädret visades");
+    };
+
 
 //EventLyssnare
 cityBtn.addEventListener("click", showWeather);
@@ -19,9 +30,22 @@ cityInput.addEventListener("change", showWeather);
 
 
 const findCityWeather=(city) => {
-    return CITIES.find(x => x.name === city)
-}
+    const findCity= CITIES.find(x => x.name === city);
+    if(!findCity) {
+      return;
+    }
+    const findLatLon = findCity.lat+","+findCity.lon;
+    const findWeather = WEATHER[findLatLon].temp;
+    const cityWeatherInfo= {
+    stad: city,
+    temperatur: WEATHER[findLatLon].temp,
+    description: WEATHER[findLatLon].description,
+    }
+    return cityWeatherInfo;
+    }
 
+    
+setInterval(showWeather, 600000);
 
 //Mockup-data (Javascript)
 const CITIES = [
