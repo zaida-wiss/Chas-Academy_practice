d582f315e525244587a639b2b289111551050922
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-11-14 13:39:48 +0100

    modulen weatherView är klar som översätter väderkoden och skapar ett väderkort med information om väder och skickar det till main

--- v46/shoppingprojekt/js/components/weatherView.js
+++ v46/shoppingprojekt/js/components/weatherView.js
@@ -1,5 +1,43 @@
+//weatherView skapar ett litet "väderkort" (ett div-element) som innehåller temperatur, väderbeskrivning och snöfall – och returnerar det till main.js som sedan visar det på sidan.
+function translateWeatherCode(code) {
+    const codes = {
+        0: "klar himmel",
+        1: "mest klart",
+        2: "delvis molnigt",
+        3: "mulet",
+        45: "dimma",
+        48: "dimma med frost",
+        51: "lätt duggregn",
+        53: "måttligt duggregn",
+        55: "kraftigt duggregn",
+        61: "lätt regn",
+        63: "måttligt regn",
+        65: "kraftigt regn",
+        71: "lätt snöfall",
+        73: "måttligt snöfall",
+        75: "kraftigt snöfall",
+        77: "snökorn",
+        80: "lätta regnskurar",
+        81: "måttliga regnskurar",
+        82: "kraftiga regnskurar",
+        85: "lätt snöby",
+        86: "kraftig snöby",
+        95: "åska",
+        96: "åska med lätt hagel",
+        99: "åska med kraftigt hagel"
+    };
+
+    return codes[code] || "okänd vädertyp";
+}
+
 
 export function weatherView(weatherData) {
-const cityInput = document.getElementById("CityInput");
-c
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <p>${weatherData.temperature}°C</p>
+    <p>${translateWeatherCode(weatherData.code)}</p>
+    <p>${weatherData.snowfall}mm</p>
+    `;
+    
+    return div;
 }
