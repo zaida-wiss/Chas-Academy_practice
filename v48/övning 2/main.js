7da858a57d90948937cb3c35b086f11c55291374
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-11-26 21:36:50 +0100

    övning 2 klar

---
+++ v48/övning 2/main.js
@@ -0,0 +1,31 @@
+console.log("Main.js laddas...");
+
+const cardContainer = document.getElementById("cardContainer");
+
+class Card {
+    constructor(img, rubrik, beskrivning) {
+        this.img = img;
+        this.rubrik = rubrik;
+        this.beskrivning = beskrivning;
+    }
+    render() {
+        const card = document.createElement("div");
+        card.classList.add("card-komponenter");
+
+        card.innerHTML = `
+        <img src="./bilder/${this.img}" alt="${this.rubrik}">
+        <h3>${this.rubrik}</h3>
+        <p>${this.beskrivning}</p>
+        <button class="cardBtn">Läs mer</button>
+        `;
+        return card;
+    }
+}
+
+const tomteTjej = new Card("nissan_pose.webp", "Tomteflicka", "Busig nisse som älskar snö.");
+const tomteKille = new Card("nisse_pose.webp", "Tomtepojke", "Glad nisse som vinkar hej!");
+const tomtenissar = new Card("nissar_bus.webp", "Busnissar", "Två nissar som busar.");
+
+cardContainer.appendChild(tomteTjej.render());
+cardContainer.appendChild(tomteKille.render());
+cardContainer.appendChild(tomtenissar.render());
