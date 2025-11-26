230d54ca66e63c754edc5630cb7b6c9c84579a69
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-11-26 19:12:18 +0100

    uppdaterat med meddelande vid lyckad inlogg

--- v48/övning 1/main.js
+++ v48/övning 1/main.js
@@ -3,16 +3,23 @@ console.log("main.js laddas");
 const loginBtn = document.getElementById("loginBtn");
 const inputUsername = document.getElementById("inputUsername");
 const inputEmail = document.getElementById("inputEmail");
+const feedback = document.createElement("div");
 
 
 loginBtn.addEventListener("click", () => {
+    feedback.textContent="";
+    feedback.id="feedback";
+    document.body.appendChild(feedback);
+
     const info = document.createElement("p");
+
     if (inputUsername.value.trim() === ""||inputEmail.value.trim() === "") {
         info.textContent = "Du har fler uppgifter att fylla i."
         info.style.backgroundColor="red";
-        document.body.appendChild(info);
+        feedback.appendChild(info);
         return;
     }
     info.textContent="du lyckades registrera dig!";
-    document.body.appendChild(info);
+    info.style.backgroundColor= "green";
+    feedback.appendChild(info);
 });
