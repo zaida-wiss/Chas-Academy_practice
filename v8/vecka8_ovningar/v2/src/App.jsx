3992e7bd5c3039f48969fcd3d957a6df6214b1f9
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-02-27 14:23:52 +0100

    nu är övning 5 klar

--- v8/vecka8_ovningar/v2/src/App.jsx
+++ v8/vecka8_ovningar/v2/src/App.jsx
@@ -2,19 +2,21 @@ import Counter from "./components/Counter/Counter";
 import Form from "./components/Form/Form";
 import Todo from "./components/Todo/Todo";
 import Posts from "./components/Posts/Posts";
+import Timer from "./components/Timer/Timer"
 import './App.css';
 
-function App() {
+export default function App() {
 
 
   return (
     <>
+      <Timer />
       <Counter />
       <Form />
       <Todo />
       <Posts />
     </>
   )
-}
+};
 
-export default App
+
---
+++ v8/vecka8_ovningar/v2/src/components/Timer/Timer.jsx
@@ -0,0 +1,74 @@
+import "./Timer.css";
+import {useState, useEffect, startTransition} from "react";
+
+
+export default function Timer () {
+  const [hour, setHour]= useState(0);
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(0);
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [laps, setLaps] = useState([]);
+
+
+  const Start =()=>{
+    setIsRunning(true);
+  };
+
+  const Stop =()=> {
+    setIsRunning(false)
+  };
+
+  const Restart =()=> {
+    setHour(0);
+    setMin(0);
+    setSec(0);
+
+  };
+
+  const Lap =() => {
+    setLaps([...laps, `${pad(hour)}:${pad(min)}:${pad(sec)}`])
+  }
+  function pad(num) {
+    return num.toString().padStart(2, "0")
+  }
+
+  useEffect(()=> {
+    let interval=null;
+    if (isRunning) {
+      interval=setInterval(() => {
+        setSec(prevSec => {
+          if (prevSec===59){
+            setMin(prevMin => {
+              if (prevMin ===59) {
+                setHour(prevHour =>prevHour +1);
+                return 0;
+              }
+              return prevMin +1
+            });
+            return 0;
+          }
+          return prevSec +1
+        });
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+    },[isRunning]);
+
+
+  return(
+    <div className="timer">
+      <div className="timer-display">
+      {pad(hour)}:{pad(min)}:{pad(sec)}</div>
+        <button onClick= {Start}>Start</button>
+        <button onClick= {Stop}>Stopp</button>
+        <button onClick= {Lap}>Varv</button>
+        <button onClick= {Restart}>Restart</button>
+        <ul>
+          {laps.map((lap, i) =>(
+            <li key= {i}>Varv {i+1}: {lap}</li>
+          ))}
+        </ul>
+      </div>
+    );
+    };
